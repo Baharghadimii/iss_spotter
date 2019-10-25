c49adffa3550092b8a4cab1c4795cf8980eee390
Baharehghadimii <bahar.ghadimi5469@gmail.com>
2019-10-25 19:09:18 +0000

    bug fixes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const { nextISSTimesForMyLocation } = require('./iss');
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
+  } else {
+    for (const pass of passTimes.response) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
   }
-  // success, print out the deets!
-  const datetime = new Date(0);
-  datetime.setUTCSeconds(passTimes.risetime);
-  const duration = passTimes.duration;
-  console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+
 });
